ed1f1f34b5eddd281b87e7c9fe9c6a0ff9b5e0e3
Sandesh Chapagain <chapagainsandesh88@gmail.com>
2026-04-27 21:12:24 -0500

    chore: remove height adjustment logic

--- frontend/src/components/chat-input.jsx
+++ frontend/src/components/chat-input.jsx
@@ -1,13 +1,7 @@
 import { Textarea } from "./ui/textarea";
 
-function ChatInput({ onChange, ...rest }) {
-  function handleChange(e) {
-    e.target.style.height = "auto";
-    e.target.style.height = e.target.scrollHeight + "px";
-    onChange?.(e);
-  }
-
-  return <Textarea onChange={handleChange} {...rest} />;
+function ChatInput(props) {
+  return <Textarea {...props} />;
 }
 
 export { ChatInput };
